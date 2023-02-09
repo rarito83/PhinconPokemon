7fe96c7c6674f9d50b7d010f56a7254e862fea5f
rarito83 <rarito.jogja@gmail.com>
2023-02-09 08:49:44 +0700

    Initial Commit

---
+++ PhinconPoke/PhinconPokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhinconPokeApp.swift
+//  PhinconPoke
+//
+//  Created by Rarito on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PhinconPokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
